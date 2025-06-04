003f81707b920a437b29db7a2dcc790529505d9c
Vekian <mathieu2991@gmail.com>
2025-06-05 01:53:05 +0200

    ajout notification in app

--- src/components/layout/header/NotificationInput.tsx
+++ src/components/layout/header/NotificationInput.tsx
@@ -58,6 +58,7 @@ export default function NotificationInput({ user }: { user: User }) {
   }, [mercureToken]);
 
   useEffect(() => {
+    console.log("test");
     if (!notifications) {
       fetchNotifications(true);
     }
@@ -70,6 +71,7 @@ export default function NotificationInput({ user }: { user: User }) {
         setMercureToken(response.data.token);
       }
       if (response.data.notifications) {
+        console.log("notifications", response.data.notifications);
         setNotifications(response.data.notifications);
       }
     }
