2f0df7b38a22a1ecd9e84968b536308e8a5e7ce4
Vekian <mathieu2991@gmail.com>
2025-06-06 00:25:29 +0200

    ajout manifest pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,24 @@ import { ThemeProvider } from "@mui/material";
 export const metadata: Metadata = {
   title: "Go Ludo",
   description: "Ludothèque et rencontre de joueurs",
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+      },
+    ],
+  },
+  appleWebApp: {
+    title: "Go Ludo",
+    capable: true,
+    statusBarStyle: "default",
+  },
 };
 
 export default function RootLayout({
