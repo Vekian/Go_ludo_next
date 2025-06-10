a1f4dffbc4081ebf95ff6443c69aedcd92e60d4e
Vekian <mathieu2991@gmail.com>
2025-06-10 06:26:18 +0200

    fix css

--- src/components/layout/header/SearchBar.tsx
+++ src/components/layout/header/SearchBar.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBarcode } from "@fortawesome/free-solid-svg-icons";
 import InputSearch from "@/components/ui/input/search/InputSearch";
 
 function SearchBar() {
